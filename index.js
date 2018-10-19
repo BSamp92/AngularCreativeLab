82e42a80f9df7280bcda5facda6d5d631b3ee232
cheyenneson <cheyenne.son@gmail.com>
2018-10-19 04:36:21 +0000

    created basic outline of website

--- index.js
+++ index.js
@@ -17,3 +17,62 @@ app.controller('myCtrl',
     }
  };
 });
+
+angular.module('app', [])
+  .controller('mainCtrl', mainCtrl)
+  .directive('avatar', avatarDirective);
+
+function mainCtrl ($scope) {
+
+  $scope.users = [];
+
+  $scope.addNew = function (user) {
+    $scope.users.push({ 
+      name: user.name,
+      url: user.url
+    }); /* [1] */
+    
+    user.name = ''; /* [2] */
+  };
+}
+
+function avatarDirective () {
+  return {
+    scope: {
+      user: '=' /* [1] */
+    },
+    restrict: 'E', /* [2] */
+    replace: 'true',
+    template: (
+      '<div class="Avatar">' +
+        '<img ng-src="{{user.avatarUrl}}" />' +
+        '<h4>{{user.name}}</h4>' +
+      '</div>'
+    ), /* [3] */
+    link: link
+  };
+  
+  function link (scope) { /* [4] */
+    if (!scope.user.avatarUrl) {
+      scope.user.avatarUrl = 'https://www.drupal.org/files/issues/default-avatar.png';
+    }
+  }
+
+}
+
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
